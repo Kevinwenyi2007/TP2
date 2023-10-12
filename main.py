b368380103cac6663c08cfe3594f9f3dd0247c80
weny <weny@educ.college-montreal.qc.ca>
2023-10-12 15:31:50 -0400

    hello

--- main.py
+++ main.py
@@ -7,24 +7,34 @@
 
 import random
 
-def game():# creer le fonction de game
-    continuer = 'yes'# si le variable qui indique de continuer le jeu est yes on recommence le boucle while
-    while continuer == 'yes':# lorsque l'utilisateur veut jouer, on commence le jeu
+# creer le fonction de jeu de number guesser
+def game():
+    # si le variable qui indique de continuer le jeu est yes on recommence le boucle while
+    continuer = 'yes'
 
-        randomNumber = random.randint(0, 100)
+    # lorsque l'utilisateur veut jouer, on commence le jeu
+    while continuer == 'yes':
+
+        min = int(input('choisi le nombre minimum de interval du jeu'))
+        max = int(input('choisi le nombre maximum de interval du jeu'))
+
+        randomNumber = random.randint(min, max)
         nombreEssais= 1
-        userNumber = int(input('donner un nombre entre 0 et 100:'))
+        userNumber = int(input('donner un nombre aleatoire de votre interval:'))
 
-        while userNumber != randomNumber:# quand la premiere reponse de l'utilisateur est fausse
+        # quand la premiere reponse de l'utilisateur est fausse et on le donne des indices pour arriver a la bonne reponse
+        while userNumber != randomNumber:
             if userNumber < randomNumber:
                 print('votre nombre est trop petit')
             elif userNumber > randomNumber:
                 print('votre nombre est trop grand')
-            nombreEssais += 1 # on ajoute 1 essai a chaque fois qu'un utilisateur a eu un mauvais reponse
+
+            nombreEssais += 1
             userNumber = int(input('donner un nombre entre 0 et 100:'))
 
-        print('bravo! tu as eu la bonne reponse en faisant', str(nombreEssais),'essais')# indication que l'utilisateur a reussi
-        continuer = str(input('est-ce que vous voulez jouer encore? yes ou no?'))# demander si l'utilisatuer veut jouer encore ou pas
+
+        print('bravo! tu as eu la bonne reponse en faisant', str(nombreEssais),'essais')
+        continuer = str(input('est-ce que vous voulez jouer encore? yes ou no?'))
 
 game()
 print('merci et au revoir')
